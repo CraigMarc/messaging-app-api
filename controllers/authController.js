eadb3e05873a6d76cee6d78e11601c41e9a7f16c
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-31 20:24:32 -0500

    sanitized sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,31 +10,88 @@ const bcrypt = require('bcryptjs')
 
 // sign up route
 
-exports.sign_up = asyncHandler(async (req, res, next) => {
-
-    //check for dupliicate usernames ************************
-    // check if passwords match *******************
-    
-    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-        // if err, do something
-        if (err) {
-            res.json({message: 'cannot encrypt'})
-            return console.log('Cannot encrypt');
-        }
-        // otherwise, store hashedPassword in DB
-        const userDetail = new User({
-
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            userName: req.body.userName,
-            password: hashedPassword
-        })
-        const user = new User(userDetail);
-        await user.save()
-        res.json({message: "user saved"})
+exports.sign_up = [
+    // Validate and sanitize the name field.
+  
+    body("firstName")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("Category name must be specified.")
+      .isAlphanumeric()
+      .withMessage("Category name has non-alphanumeric characters."),
+    body("lastName")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("Description must be specified.")
+      .isAlphanumeric()
+      .withMessage("Description has non-alphanumeric characters."),
+    body("userName")
+      .trim()
+      .isLength({ min: 4 })
+      .escape()
+      .withMessage("UserName must be 4 characters."),
+      
+    body("password")
+      .trim()
+      .isLength({ min: 6 })
+      .escape()
+      .withMessage("Password must be at least 6 characters."),
+    body('confirm').custom((value, { req }) => {
+      return value === req.body.password;
     })
+      .withMessage("Passwords must match."),
+  
+  
+  
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+  
+  
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render the form again with sanitized values/error messages.
+        
+        res.json({errors: errors.array() })
+        return;
+      } 
+      
+      else {
+        // Data from form is valid.
+        // Check if Username already exists.
+        const userNameExists = await User.findOne({ userName: req.body.userName }).exec();
+        if (userNameExists) {
+          // UserName exists, redirect to its detail page.
+          
+          res.json({errors: "user name already exists"})
+        } else {
+         
+          bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+            // if err, do something
+            if (err) {
+                res.json({message: 'cannot encrypt'})
+                return console.log('Cannot encrypt');
+            }
+            // otherwise, store hashedPassword in DB
+            const userDetail = new User({
+    
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                userName: req.body.userName,
+                password: hashedPassword
+            })
+            const user = new User(userDetail);
+            await user.save()
+            res.json({message: "user saved"})
+        })
+        }
+      }
+    }),
+  ];
 
-})
 
 // log in route
 
