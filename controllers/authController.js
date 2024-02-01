0745a0defab4ce7d0bb3e595063cbaf97c152f24
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2024-01-31 19:55:49 -0500

    added authcontroller

---
+++ controllers/authController.js
@@ -0,0 +1,66 @@
+//const Comments = require("../models/comments");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require("../models/user");
+const bcrypt = require('bcryptjs')
+
+// sign up route
+
+exports.sign_up = asyncHandler(async (req, res, next) => {
+    
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        // if err, do something
+        if (err) {
+            res.json({message: 'cannot encrypt'})
+            return console.log('Cannot encrypt');
+        }
+        // otherwise, store hashedPassword in DB
+        const userDetail = new User({
+
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userName: req.body.userName,
+            password: hashedPassword
+        })
+        const user = new User(userDetail);
+        await user.save()
+        res.json({message: "user saved"})
+    })
+
+})
+
+// log in route
+
+exports.log_in = asyncHandler(async (req, res, next) => {
+    //exports.log_in = (req, res) => {
+    let { email, password } = req.body;
+    try {
+        let userDb = await User.find({ 'userName': email }).exec()
+
+        const match = await bcrypt.compare(password, userDb[0].password);
+
+        if (userDb[0].userName === email) {
+            if (match == true) {
+                //if (userDb[0].password === password) { //the password compare would normally be done using bcrypt.
+                const opts = {}
+                opts.expiresIn = 1200;  //token expires in 2min
+                const secret = process.env.SECRET_KEY
+                const token = jwt.sign({ email }, secret, opts);
+                return res.status(200).json({
+                    message: "Auth Passed",
+                    token
+                })
+            }
+        }
+
+    } catch (error) {
+        res.status(500).json({ message: "wrong username or password" });
+    }
+
+    return res.status(401).json({ message: "Auth Failed" })
+})
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
+/*
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+module.exports = router;
+*/
+const express = require("express");
+const router = express.Router();
+
+// Require controller modules.
+//const nonUser_controller = require("../controllers/nonUserController");
+const auth_controller = require("../controllers/authController");
+
+/// NonUser ROUTES ///
+
+//sign up
+
+router.post("/signup/", auth_controller.sign_up);
+
+// login 
+
+router.post("/login/", auth_controller.log_in);
+
+/*
+
+// Get all published posts
+
+router.get("/published", nonUser_controller.all_published_posts_get);
+
+// GET all comments.
+router.get("/comments/", nonUser_controller.all_comments_get);
+
+// get comments for specific post
+
+router.get("/comments/:postId", nonUser_controller.post_comments_get);
+
+//add commment to post
+
+router.post("/comments/", nonUser_controller.comments_create);
+
+// login 
+
+router.post("/login/", auth_controller.log_in);
+
+// logout
+
+router.post("/logout/", auth_controller.log_out);
+*/
 
 module.exports = router;
